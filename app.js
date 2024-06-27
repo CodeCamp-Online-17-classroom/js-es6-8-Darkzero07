286ad4d30399cd0ebd865ce4b6c8b8b6d3827abd
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-27 22:16:09 +0700

    js lab 152

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+// จงเขียนฟังก์ชัน removeRandom โดยรับพารามิเตอร์มาเป็น Array และ return ค่าเป็น Array ที่มี element ถูกลบออกไปหนึ่งตัวโดยตำแหน่งที่ลบให้เกิดจากการสุ่ม
+function removeRandom(array) {
+  const indexOfRmoveArray = Math.round(Math.random() * array.length);
+//   console.log(indexOfRmoveArray);
+  return array.filter((arr, index) => index !== indexOfRmoveArray);
+}
+const originalArray = [1, 2, 3, 4, 5];
+const modifiedArray = removeRandom(originalArray);
+
+console.log(originalArray); // [1, 2, 3, 4, 5]
+console.log(modifiedArray); // [1, 2, 4, 5]
